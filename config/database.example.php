2813ddf576e8bdd4bf83bd02bf5e97cc7610b080
vararunas <79661127+vararunas@users.noreply.github.com>
2026-09-18 18:36:24 +0300

    Add secure MySQL economy persistence

---
+++ config/database.example.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+return [
+ 'host' => 'sql307.infinityfree.com',
+ 'port' => 3306,
+ 'database' => 'if0_42949690_mygame',
+ 'username' => 'if0_42949690',
+ 'password' => 'PAKEISK_MANĘ_SERVERYJE',
+ 'charset' => 'utf8mb4',
+];
