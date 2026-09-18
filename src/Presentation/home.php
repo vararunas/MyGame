bfb3600cd7dc64d91e6234aecb1bb53d6c65d188
vararunas <79661127+vararunas@users.noreply.github.com>
2026-09-18 15:34:18 +0300

    Fix city marker positioning on Lithuania map

--- src/Presentation/home.php
+++ src/Presentation/home.php
@@ -8,7 +8,7 @@
 <svg class="country" viewBox="0 0 1000 650" aria-label="Lietuvos žemėlapis"><path d="M54 344 L82 278 L113 205 L161 143 L235 124 L302 78 L392 94 L475 58 L568 82 L647 62 L744 105 L834 103 L907 157 L928 237 L891 307 L922 382 L870 455 L799 476 L742 548 L650 563 L575 608 L480 579 L400 603 L321 558 L233 570 L170 514 L109 489 L87 409 Z"/></svg>
 <div class="map-tooltip" id="mapTooltip"></div>
 <?php foreach ($cities as $city): $size=max(8,min(24,7+log(max($city->population,5000)/5000,1.8))); $major=$city->population>=80000; ?>
-<div class="city-marker" style="left:<?= $city->x ?>%;top:<?= $city->y ?>%">
+<div class="city-marker" style="--x:<?= $city->x ?>%;--y:<?= $city->y ?>%">
 <button class="city-dot" style="width:<?= $size ?>px;height:<?= $size ?>px" data-city="<?= htmlspecialchars($city->name) ?>" data-pop="<?= $city->population ?>" aria-label="<?= htmlspecialchars($city->name) ?>"></button>
 <?php if($major): ?><span class="city-label"><?= htmlspecialchars($city->name) ?></span><?php endif; ?></div>
 <?php endforeach; ?></div></div>
